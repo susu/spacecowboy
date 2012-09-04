a7000947bae117f8aea9c66b51ba397977a19cbd
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2012-09-04 19:09:05 +0200

    CollisionEvent class implemented

---
+++ include/sc/phi/CollisionEvent.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <sc/evt/Event.hpp>
+#include <sc/phi/BaseTypes.hpp>
+
+#include <vector>
+
+namespace sc
+{
+  namespace phi
+  {
+    class Model;
+
+    class CollisionEvent : public sc::evt::Event
+    {
+      public:
+        CollisionEvent( const ObjectRef& collidingObject, Model& physicalModel );
+        virtual void handle( sc::evt::Registry& registry );
+
+        Coordinate otherSpeed() const;
+        Coordinate otherCoord() const;
+
+      private:
+        ObjectRef m_collidingObject;
+        Model& m_modelOfCollider;
+    };
+  }
+}
+
---
+++ src/phi/CollisionEvent.cpp
@@ -0,0 +1,32 @@
+#include <sc/phi/CollisionEvent.hpp>
+#include <sc/phi/EventSlots.hpp>
+#include <sc/phi/Model.hpp>
+#include <sc/evt/Event.hpp>
+
+sc::phi::CollisionEvent::CollisionEvent( const ObjectRef& collidingObject, Model& physicalModel )
+  : sc::evt::Event()
+  , m_collidingObject( collidingObject )
+  , m_modelOfCollider( physicalModel )
+{
+}
+
+void
+sc::phi::CollisionEvent::handle( sc::evt::Registry& registry )
+{
+  notifySlot( registry, slot::COLLISION );
+}
+
+
+sc::phi::Coordinate
+sc::phi::CollisionEvent::otherSpeed() const
+{
+  return m_modelOfCollider.speed();
+}
+
+
+sc::phi::Coordinate
+sc::phi::CollisionEvent::otherCoord() const
+{
+  return m_modelOfCollider.coordinate();
+}
+
