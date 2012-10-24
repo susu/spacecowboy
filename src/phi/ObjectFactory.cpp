7f31bc8212835d868bdbdd6419d54471d7383af3
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2012-10-24 10:47:51 +0200

    lgpl licence and header added

--- src/phi/ObjectFactory.cpp
+++ src/phi/ObjectFactory.cpp
@@ -1,3 +1,25 @@
+/*
+ * spacecowboy - a 2D space adventure game
+ *
+ *  Copyright 2012, Peter Ferenc Hajdu.
+ *  All rights reserved.
+ *
+ * This file is part of .
+ *
+ * spacecowboy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * spacecowboy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with spacecowboy. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 #include <sc/phi/BaseTypes.hpp>
 #include <sc/phi/ObjectFactory.hpp>
 
